c9a67d9cc3696f3775c76181ef2aff5f1499b855
Subhasishbasak <subhasish.basak@anses.fr>
2024-09-03 14:32:32 +0200

    WIP: environmental swimming submodule

---
+++ submodule_swimming.R
@@ -0,0 +1,16 @@
+## Submodule for recreational swimming
+
+#concentration after (no) decay in worst case scenario
+data$C_decay.swimming <- data$C_init
+
+#concentration in bathing sites
+data$C_bath <- (data$C_decay.drinking * data$F)  / data$V
+  
+#human exposure
+if(data$swimmer_type[1] == 1){
+  w_ingest <- data$w_ingest.adult
+} else {
+  w_ingest <- data$w_ingest.child
+}
+
+data$dose_swimming <- data$C_bath * w_ingest
--- utilities/decay_model.R
+++ utilities/decay_model.R
@@ -2,7 +2,7 @@
 
 get_decay_rate <- function(params) {
   k <-
-    (0.8 + 0.006 * params$P_sal) * 1.07 ^ (params$T - 20) + params$IA * (1 - exp(-params$et * params$H)) / (params$et * params$H)
+    (0.8 + 0.006 * params$sal) * 1.07 ^ (params$T - 20) + params$IA * (1 - exp(-params$et * params$H)) / (params$et * params$H)
   
   return(k)
 }
